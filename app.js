60d66e587ed796d04a3d8d7d9db5bb39bcc6d8ea
Andres Cruz <andreselias1404@gmail.com>
2023-03-25 17:15:53 -0600

    Repaso de conocimientos previos, mostrar la tabla del 5 haciendo uso de un for y un text template

---
+++ app.js
@@ -0,0 +1,9 @@
+console.clear();
+console.log('================');
+console.log('Tabla del 5');
+console.log('================');
+// Imprimir la tabla del 5 en la consola
+const base = 5;
+for (let index = 1; index <= 10; index++) {
+    console.log(`${base} x ${index} = ${index*base} `)
+}

ca1e10474f23b6c456456e1b2fd27e216f3aadc2
Andres Cruz <andreselias1404@gmail.com>
2023-03-25 18:09:04 -0600

    Hacer uso de librerias con el require.

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+// De que manera realizar un archivo de texto para guardar el documento de texto con los resultados
+// Se utilizaria el paquete file system para crear archivos
+const fs = require('fs');
+
+let salida= '';
+const base = 1;
 console.clear();
 console.log('================');
-console.log('Tabla del 5');
+console.log(`Tabla del ${base}`);
 console.log('================');
 // Imprimir la tabla del 5 en la consola
-const base = 5;
 for (let index = 1; index <= 10; index++) {
-    console.log(`${base} x ${index} = ${index*base} `)
+    salida+=`${base} x ${index} = ${index*base}\n`;
 }
+console.log(salida);
+fs.writeFile(`tabla_${base}.txt`, salida, (err)=>{
+    if (err) throw err;
+    console.log('Archivo creado');
+});

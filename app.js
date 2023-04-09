1b9e77f7f793a364549a8559d8d8f970e6d10b23
Andres Cruz <andreselias1404@gmail.com>
2023-04-09 10:56:19 -0600

    Manejar argumentos enviados por linea de comandos de manera manual

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const {crearArchivo} = require("./helpers/multiplicar")
-const base = 9;
-console.clear();
+//const base = 9;
+console.log(process.argv)
+const [,,arg3='base=5'] = process.argv;
+const [,base] = arg3.split('=')
+console.log(base)
+//console.clear();
 crearArchivo(base)
     .then(nombreArchivo => console.log(nombreArchivo, "creado"))
     .catch( err => console.log(err));
